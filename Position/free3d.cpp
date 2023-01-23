085777f7a5ad5d2f803f805ae97585d48738568e
SachaAmiel <sacha.amiel@universite-paris-saclay.fr>
2023-01-23 17:49:15 +0100

    New module free3d

---
+++ Position/free3d.cpp
@@ -0,0 +1,157 @@
+#include "position.h"
+#include "free3d.h"
+#include "../Point_et_Vec/Point_et_Vec.h"
+#include <cmath>
+#include <iostream>
+#include <stdexcept>
+using namespace std;
+
+free3d::free3d (Imp X, Rot A){
+  x_coord = X.x;
+  y_coord = X.y;
+  z_coord = X.z;
+  angle = A;
+}
+
+void free3d::operator+= (real_t v){
+  x_coord+=v*sin(angle.theta())*cos(angle.phi());
+  y_coord+=v*sin(angle.theta())*sin(angle.phi());
+  z_coord+=v*cos(angle.phi());
+}
+
+void free3d::operator+= (Imp v){
+  Imp imp;
+  imp = v.rotate(angle)
+  x_coord+=imp.x;
+  y_coord+=imp.y;
+  z_coord+=imp.z;
+}
+
+void free3d::operator^ (Rot a){
+  angle+= a;
+}
+
+real_t free3d::operator| (free3d Y){
+  return sqrt( (x_coord-Y.x_coord)*(x_coord-Y.x_coord)
+             + (y_coord-Y.y_coord)*(y_coord-Y.y_coord)
+             + (z_coord-Y.z_coord)*(z_coord-Y.z_coord) );
+}
+
+bool free3d::order (int axis, free3d Y){
+  if (axis==1) return x_coord<Y.x_coord;
+  if (axis==2) return y_coord<Y.y_coord;
+  if (axis==3) return z_coord<Y.z_coord;
+  // case of an unexisting axis
+  cout << endl << endl
+       << "You requested the 'order' method on axis "
+       << axis << endl << "There is no such axis" << endl;
+  throw std::invalid_argument( "Unexisting axis" );
+}
+
+Imp  free3d::operator-  (free3d Y   ){
+  return {x_coord-Y.x_coord, y_coord-Y.y_coord, z_coord-Y.z_coord};
+}
+
+
+Rot free3d::operator<< (free3d Y){
+  return (Y-this[0]).direction()+angle;
+}
+
+// fonction to know how to scale in the square projection
+real_t dil_sqr(free3d X){
+  if (abs(X.x)<0.5 && abs(X.y)<0.5 && abs(X.z)<0.5){return 1.;}
+  real_t m;           m = abs(X.x);
+  if (m<abs(X.y)){    m = abs(X.y);}
+  if (m<abs(X.z)){ return abs(X.z);}
+  if (m==abs(X.x)){return abs(X.x)}
+                   return abs(X.y);
+}
+
+// !!!! Think !!!!!!
+real_t free3d::x (int proj){
+  if (proj==0) {
+    real_t d = 1. - 1./16 ;
+    //≈0.9375 ie closse to 0.99 but with exact binary representation
+    // made to ensure that a boid doesn't get too close to the edge 
+    // because we don't want edge cases when going into pixels
+    return x_coord/dil_sqr(*this)*d/2 + 0.5;
+  }
+
+  if (proj==1) return x_coord;
+
+  if (proj==2 && x_coord>=0) return 0.5 + (1-exp(-x_coord))/2;
+  if (proj==2)         return 0.5 + (exp(x_coord) -1)/2;
+
+  // case of an unexisting projection
+  cout << endl << endl
+       << "You requested the 'x' method with projection "
+       << proj << endl << "There is no such projection" 
+       << endl << "Refer to free3d.h 's documentation"<< endl;
+  throw std::invalid_argument( "Unexisting projection" );
+}
+
+real_t free3d::y (int proj){
+  if (proj==0) {
+    real_t d = 1. - 1./16 ;
+    //≈0.9375 ie closse to 0.99 but with exact binary representation
+    // made to ensure that a boid doesn't get too close to the edge 
+    // because we don't want edge cases when going into pixels
+    return y_coord/dil_sqr(*this)*d/2 + 0.5;
+  }
+
+  if (proj==1) return y_coord;
+
+  if (proj==2 && y_coord>=0) return 0.5 + (1-exp(-y_coord))/2;
+  if (proj==2)         return 0.5 + (exp(y_coord) -1)/2;
+
+  // case of an unexisting projection
+  cout << endl << endl
+       << "You requested the 'y' method with projection "
+       << proj << endl << "There is no such projection" 
+       << endl << "Refer to free3d.h 's documentation"<< endl;
+  throw std::invalid_argument( "Unexisting projection" );
+}
+
+real_t free3d::z (int proj){
+  if (proj==0) {
+    real_t d = 1. - 1./16 ;
+    //≈0.9375 ie closse to 0.99 but with exact binary representation
+    // made to ensure that a boid doesn't get too close to the edge 
+    // because we don't want edge cases when going into pixels
+    return z_coord/dil_sqr(*this)*d/2 + 0.5;
+  }
+
+  if (proj==1) return z_coord;
+
+  if (proj==2 && z_coord>=0) return 0.5 + (1-exp(-z_coord))/2;
+  if (proj==2)         return 0.5 + (exp(z_coord) -1)/2;
+
+  // case of an unexisting projection
+  cout << endl << endl
+       << "You requested the 'y' method with projection "
+       << proj << endl << "There is no such projection" 
+       << endl << "Refer to free3d.h 's documentation"<< endl;
+  throw std::invalid_argument( "Unexisting projection" );
+}
+
+real_t free3d::t (int proj){
+  if (proj==0 || proj==1 || proj==2) return angle.theta;
+
+  // case of an unexisting projection
+  cout << endl << endl
+       << "You requested the 't' method with projection "
+       << proj << endl << "There is no such projection" 
+       << endl << "Refer to free3d.h 's documentation"<< endl;
+  throw std::invalid_argument( "Unexisting projection" );
+}
+
+real_t free3d::p (int proj){
+  if (proj==0 || proj==1 || proj==2) return angle.phi;
+
+  // case of an unexisting projection
+  cout << endl << endl
+       << "You requested the 'p' method with projection "
+       << proj << endl << "There is no such projection" 
+       << endl << "Refer to free3d.h 's documentation"<< endl;
+  throw std::invalid_argument( "Unexisting projection" );
+}
---
+++ Position/free3d.h
@@ -0,0 +1,81 @@
+#ifndef _FREE3D_H_
+#define _FREE3D_H_
+#include "../Vec/Vec.h"
+#include "position.h"
+
+class free3d {
+//class free3d : public pos {
+ public:
+  free3d (Imp X, Rot A);
+  
+  void   operator+= (real_t v   );
+  void   operator+= (Imp    v   );
+  void   operator^  (Rot    a   );
+  real_t operator|  (free3d Y   );
+  bool   order      (int    axis,
+              	    free3d  Y   );
+  Imp   operator-  (free3d  Y   );
+  Rot   operator<< (free3d  Y   );
+  real_t x          (int    proj);
+  real_t y          (int    proj);
+  real_t z          (int    proj);
+  real_t t          (int    proj);
+  real_t p          (int    proj);
+  
+  void   move       (Imp    v){       this->operator+=( v);};
+  void   move       (real_t v){       this->operator+=( v);};
+  void   operator-= (Imp    v){       this->operator+=(-v);};
+  void   operator-= (real_t v){       this->operator+=(-v);};
+  void   turn       (Rot    a){       this->operator^ ( a);};
+  real_t dist       (free3d Y){return this->operator| ( Y);};
+  Imp    move_view  (free3d Y){return this->operator- ( Y);};
+  Rot    angle_view (free3d Y){return this->operator<<( Y);};
+  Rot    operator>> (free3d Y){return  Y<<(this[0]);};
+  real_t x (){return this->x(0);};
+  real_t y (){return this->y(0);};
+  real_t z (){return this->z(0);};
+  real_t t (){return this->t(0);};
+  real_t p (){return this->p(0);};
+
+ private:
+  real_t x_coord;
+  real_t y_coord;
+  real_t z_coord;
+  Rot angle; // stands for (θ, \phi)
+};
+
+#endif
+
+
+/* free3d Documentation :
+
+Projections:
+  0) {default} cropped projection
+    x,y,z ∈ ]0 , 1[³
+    t   ∈ ]-π, π]
+    p   ∈ ] 0, π]
+
+    When a boid leaves [-½,½]³,
+      we only get its projection on the square's
+      edges.
+      (all of that is shifted by (½,½,½) to center 
+      the picture on (0,0,0), for aesthetics)
+    This allows to follow their orentation without
+      leaving the center square...
+  
+
+  1) trivial projection
+    x,y,z ∈ ℝ³
+    t     ∈ ]-π, π]
+    p     ∈ ]0,  π]
+
+
+  2) exponantial projection
+      x,y,z ∈ ]0, 1[²
+      t     ∈ ]-π, π]
+      p     ∈ ]2, π]
+
+    Gives a global view of the entire ℝ³
+      (all of that is shifted by (½,½,½) to center 
+      the picture on (0,0), for aesthetics)
+*/
