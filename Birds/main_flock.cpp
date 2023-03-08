882b4aef2e5b9b5ecf0e019c91c1399018b0c56c
Leo Constantin <leo.constantin@universite-paris-saclay.fr>
2023-03-08 12:16:25 +0100

    testing the update_flock method

--- Birds/main_flock.cpp
+++ Birds/main_flock.cpp
@@ -1,44 +1,52 @@
 #include<iostream>
-#include "../Boids.h"
 #include "Flock.h"
 using namespace std;
 int main (){
 	sf::RenderWindow window(sf::VideoMode(1900, 1600), "BOIDS");
 	window.setFramerateLimit(30);
-    Flock flock_piaf (3,0,0,0,0);
+    Flock flock_piaf (100,0,1,0,0);
     std::vector<pos> l_piafs = flock_piaf.pos_boids();
 	int i;
 	for (i=0;i<3;i++) {
 		cout << "posi.x() " << l_piafs[i].x() << " " << l_piafs[i].y() << endl;
 	}
-	cout<<"x "<<l_piafs[0].x()<<" "<<l_piafs[0].y()<<endl;
-	cout<<"x "<<l_piafs[1].x()<<" "<<l_piafs[1].y()<<endl;
-	cout<<"x "<<l_piafs[2].x()<<" "<<l_piafs[2].y()<<endl;
+	cout<<"xgen "<<l_piafs[0].x()<<" "<<l_piafs[0].y()<<endl;
+	cout<<"xgen "<<l_piafs[1].x()<<" "<<l_piafs[1].y()<<endl;
+	cout<<"xgen "<<l_piafs[2].x()<<" "<<l_piafs[2].y()<<endl;
 	int j;
 	for (i=0;i<3;i++) {
-		cout << "d_ij ";
+		cout << "d_ijgen ";
 		for (j=0;j<i+1;j++) {
 			cout << flock_piaf.tab_dist()[i][j] << " ";
 		}
 		cout << endl;
 	}
-	
+	int etape=0;
 	while (window.isOpen()){
+		etape+=1;
+		cout << "etape " << etape << endl;
 		window.clear(sf::Color::Yellow);
 		sf::Event event;
 		while (window.pollEvent(event)){
 			if (event.type == sf::Event::Closed)
 				window.close();
 			}
-			/*switch(event.type)
-			{
-				case sf::Event::Closed:
-					window.close();
-					break;
-				//default:
-				//	break; 
-				}}*/
 		window.clear(sf::Color::Yellow);
+		flock_piaf.update_flock();
+		for (i=0;i<3;i++) {
+		cout << "posi.x() " << l_piafs[i].x() << " " << l_piafs[i].y() << endl;
+	}
+		cout<<"x "<<l_piafs[0].x()<<" "<<l_piafs[0].y()<<endl;
+		cout<<"x "<<l_piafs[1].x()<<" "<<l_piafs[1].y()<<endl;
+		cout<<"x "<<l_piafs[2].x()<<" "<<l_piafs[2].y()<<endl;
+		for (i=0;i<3;i++) {
+			cout << "d_ij ";
+			for (j=0;j<i+1;j++) {
+				cout << flock_piaf.tab_dist()[i][j] << " ";
+			}
+			cout << endl;
+		}
+		
 		flock_piaf.draw(window);
         //flock_piaf.draw(window);
 		window.display();
